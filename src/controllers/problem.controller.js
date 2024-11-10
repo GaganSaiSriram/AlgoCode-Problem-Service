bf95a33110307865f06c840746b1fb8dd5656d1a
Gagan Sai Sri Ram <sr3524514@gmail.com>
2024-11-10 23:38:55 +0530

    Feat:Added error handler middleware with basic error handle classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,44 +1,67 @@
-
-const {StatusCodes} = require('http-status-codes');
+const NotImplemented = require('../errors/notimplemented.error');
 
 function pingProblemController(req,res){
     res.json({message:"Ping Problem controller is up!!"})
 }
 
-function addProblem(req,res){
+function addProblem(req,res,next){
 
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message:"Not implemenred"   
-    })
+    try {
+        // nothing implemented
+
+        throw new NotImplemented('addProblem');
+    }catch(error){
+        next(error);
+
+    }
 
 }
 
 function getProblem(req,res){
 
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message:"Not implemenred"   
-    })
+    try {
+        // nothing implemented
+
+        throw new NotImplemented('getProblem');
+    }catch(error){
+        next(error);
+
+    }
 
 }
 
 function getProblems(req,res){
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message:"Not implemenred"   
-    })
+    try {
+        // nothing implemented
 
+        throw new NotImplemented('getProblems');
+    }catch(error){
+        next(error);
+
+    }
 }
 
 function deleteProblem(req,res){
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message:"Not implemenred"   
-    })
+    try {
+        // nothing implemented
+
+        throw new NotImplemented('deleteProblem');
+    }catch(error){
+        next(error);
+
+    }
 
 }
 
 function updateProblem(req,res){
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message:"Not implemenred"   
-    })
+    try {
+        // nothing implemented
+
+        throw new NotImplemented('updateProblem');
+    }catch(error){
+        next(error);
+
+    }
     
 }
 
